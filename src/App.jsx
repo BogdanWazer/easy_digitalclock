79da579d6cbd07afbc8f0de8123f9656302b16d5
Bohdan Wazer <ki4ack.bogdan@gmail.com>
2024-01-08 22:12:09 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+import DigitalClock from './DigitalClock';
+function App() {
+  return (
+    <>
+      <DigitalClock></DigitalClock>
+    </>
+  );
+}
+
+export default App;
